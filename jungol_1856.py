6d98d4b264b24ac3a98f0578c71b19ee2376bf8f
gagreen <minsang6088@naver.com>
2020-01-06 21:28:04 +0900

    2020_01_06

---
+++ jungol_1856.py
@@ -0,0 +1,12 @@
+n, m = input().split()
+n = int(n)
+m = int(m)
+
+for i in range(1, n+1):
+    if i%2 != 0 :
+        for j in range((i-1)*m+1, m*i+1):
+            print(j, end=' ')
+    else :
+        for j in range(i*m, (i-1)*m, -1):
+            print(j, end=' ')
+    print('')
