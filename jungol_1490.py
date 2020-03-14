c5decf5f421b3d70cbd8bc7f999f9025a516db98
gagreen <minsang6088@naver.com>
2020-03-14 21:34:27 +0900

    2020_03_08
    Resolving pending issues When 2020_03_14

--- jungol_1490.py
+++ jungol_1490.py
@@ -4,13 +4,13 @@ n, k = input("").split()
 n = int(n)
 k = int(k)
 data = input("").split()
-data.sort()
 data = ' '.join(data)
 
 com = list(map(' '.join, combinations([str(i) for i in range(1,n+1)], k)))
-
+# 각각의 조합을 만들어 문자열로 
 check = 0
 for i in range(len(com)):
+    print(data, com[i])
     if data == com[i]:
         check = 1
         print(com[i+1])
