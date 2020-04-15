a17b5ac81d51be2b232454bb7ab2a4254c7619a7
gagreen <minsang6088@naver.com>
2020-04-15 22:35:30 +0900

    2020_04_15

---
+++ jungol_170.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print(){
+	printf("@@@@@@@@@@\n");
+}
+
+int main(){
+	printf("first\n");
+	print();
+	printf("second\n");
+	print();
+	printf("third\n");
+	print();
+	
+}
