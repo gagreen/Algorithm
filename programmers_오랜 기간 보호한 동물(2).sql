64fbf3a985b413b0b7ae42c678567462369d79cd
minseong <minsang6088@naver.com>
2020-07-21 18:28:41 +0900

    2020_07_21

---
+++ programmers_오랜 기간 보호한 동물(2).sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME
+    FROM (SELECT ai.ANIMAL_ID, ai.NAME
+        FROM ANIMAL_INS ai, ANIMAL_OUTS ao
+        WHERE ai.ANIMAL_ID = ao.ANIMAL_ID 
+        ORDER BY ao.DATETIME - ai.DATETIME DESC)
+    WHERE ROWNUM <= 2
