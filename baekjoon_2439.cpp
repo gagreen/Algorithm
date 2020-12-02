dbc0680ecdda84686e51e9cabca7d8f50bd5fa8b
minseong <minsang6088@naver.com>
2020-12-02 14:00:44 +0900

    Create baekjoon_2439.cpp

---
+++ baekjoon_2439.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	int i, j;
+	
+	scanf("%d", &n);
+	
+	for(i=1; i<=n; i++){
+		for(j=0; j<n-i; j++) {
+			printf(" ");
+		}
+		
+		for(j=0; j<i; j++){
+			printf("*");
+		}
+		
+		printf("\n");
+	}
+}
